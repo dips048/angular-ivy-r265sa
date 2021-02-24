59b68a611274c7fa13ca2e55bc5210008a645d92
dips048 <dipeshpatel048@gmail.com>
2021-02-24 11:56:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-r265sa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-r265sa)
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+
+import { StoreModule } from "@rxjs/store";
+import { counterReducer } from "./counter.reducer";
+import { MyCounterComponent } from './my-counter/my-counter.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ count: counterReducer })
+  ],
+  declarations: [AppComponent, HelloComponent, MyCounterComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
